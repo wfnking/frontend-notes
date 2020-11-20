472285ff3f0fbf819594389ecf46589aee7f2fe5
fning <wfnking@gmail.com>
2020-11-20 11:34:02 +0800

    修复： import报错

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 // pages/_app.js
-import '../styles/tailwind.css'
-import 'typeface-open-sans'
+import "../styles/tailwind.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head></Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
